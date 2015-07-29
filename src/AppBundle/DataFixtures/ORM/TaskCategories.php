7faa598ab94249431696c8e4ab483c2d58215066
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-07-29 13:26:56 +0200

    Created TaskCategories fixtures

---
+++ src/AppBundle/DataFixtures/ORM/TaskCategories.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\TaskCategory;
+
+class TaskCategories extends AbstractFixture implements OrderedFixtureInterface
+{
+  public function getOrder(){
+    return 30;
+  }
+
+  public function load(ObjectManager $manager)
+  {
+    $categories = array(
+      'project',
+      'budget',
+      'sat',
+      'private'
+    );
+
+    foreach($categories as $TaskCategories)
+    {
+      $categories = new TaskCategory();
+      $categories->setTaskCategory($TaskCategories);
+
+      $manager->persist($categories);
+    }
+
+    $manager->flush();
+  }
+}
