da3456f0cb2b6011469142b7fdcdccd69d18f84e
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-07-30 11:57:47 +0200

    Created Users Fixtures

---
+++ src/AppBundle/DataFixtures/ORM/Users.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AppBundle\Entity\User;
+
+class Users extends AbstractFixture implements
+  OrderedFixtureInterface, ContainerAwareInterface
+{
+  private $container;
+
+  public function getOrder()
+  {
+    return 40;
+  }
+
+  public function setContainer(ContainerInterface $container = null)
+  {
+    $this->container = $container;
+  }
+
+  public function load(ObjectManager $manager)
+  {
+    $userManager = $this->container->get('fos_user.user_manager');
+
+    $user = $userManager->createUser();
+    $user->setUsername('username');
+    $user->setEmail('user@crm.es');
+    $user->setPlainPassword('password');
+    $user->setEnabled(true);
+    $user->setRoles(array('ROLE_ADMIN'));
+
+    $userManager->updateUser($user, true);
+  }
+}
