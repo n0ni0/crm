ccd8f666639c4e7b387a631c1917606cb6f53931
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-08-11 23:53:36 +0200

    Unit test findContactProfile

---
+++ src/AppBundle/Tests/Entity/ContactRepositoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Tests\Entity;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ContactRepositoryTest extends kernelTestCase
+{
+  /**
+   * @var \Doctrine\ORM\EntityManager
+   */
+  private $em;
+
+  public function setUp()
+  {
+    self::bootKernel();
+    $this->em = static::$kernel->getContainer()
+      ->get('doctrine')
+      ->getManager();
+  }
+
+  public function testFindContactProfile()
+  {
+    $contacts = $this->em
+      ->getRepository('AppBundle:Contact')
+      ->findContactProfile('5');
+
+    $this->assertCount(1, $contacts);
+  }
+}
