3963919c36c51afb90a40697fee74319de903276
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-09-19 15:44:17 +0200

    Added Paginator on task list

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -5,21 +5,29 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Utils\Constants;
 
 class TaskController extends Controller
 {
   /**
    * @Route("/task", name="listTask")
    */
-  public function listAction()
+  public function listAction(Request $request)
   {
     $task = $this->get('TaskManager')->findAllTasks();
 
     if(!$task){
       throw $this->createNotFoundException('Task not found');
     }
+
+      $paginator  = $this->get('knp_paginator');
+      $tasks   = $paginator->paginate(
+      $task,
+      $request->query->getInt('page',1), $pages = Constants::CONTACTS_PER_PAGE
+      );
     return $this->render('tasks/taskList.html.twig', array(
-      'task' => $task
+      'task'  => $task,
+      'tasks' => $tasks
     ));
   }
 
