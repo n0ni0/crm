da94c0f519298083b2622f93ef48bfa6d72dc89b
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-10-07 16:12:05 +0200

    Removed unused classes

--- src/AppBundle/Form/Type/CommentType.php
+++ src/AppBundle/Form/Type/CommentType.php
@@ -5,8 +5,6 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 
 class CommentType extends AbstractType
 {
