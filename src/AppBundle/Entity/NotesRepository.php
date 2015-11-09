a6648bc8c76a3f73e37033a8ead229bd1326207f
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-11-09 23:02:34 +0100

    Limit number of notes on main page

--- src/AppBundle/Entity/NotesRepository.php
+++ src/AppBundle/Entity/NotesRepository.php
@@ -19,6 +19,20 @@ class NotesRepository extends EntityRepository
    return $query->getResult();
   }
 
+  public function findPublicNotesMain($private)
+  {
+   $em  = $this->getEntityManager();
+   $dql = 'SELECT n
+             FROM AppBundle:Notes n
+            WHERE n.private = :private';
+
+   $query = $em->createQuery($dql);
+   $query->setParameter('private', $private);
+   $query->execute();
+   $query->setMaxResults('5');
+   return $query->getResult();
+  }
+
   public function findPrivateNotes($user, $private)
   {
    $em  = $this->getEntityManager();
@@ -31,6 +45,22 @@ class NotesRepository extends EntityRepository
    $query->setParameter('user', $user);
    $query->setParameter('private', $private);
    $query->execute();
+   return $query->getResult();
+  }
+
+  public function findPrivateNotesMain($user, $private)
+  {
+   $em  = $this->getEntityManager();
+   $dql = 'SELECT n.title, n.id
+              FROM AppBundle:Notes n
+             WHERE n.user = :user
+               AND n.private = :private';
+
+   $query = $em->createQuery($dql);
+   $query->setParameter('user', $user);
+   $query->setParameter('private', $private);
+   $query->execute();
+   $query->setMaxResults('5');
    return $query->getResult();
   }
 
