59cbb85a441d6777f0a2f4ae732e1b6c8fa5e788
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-11-12 16:27:44 +0100

    Updated the obsolete code

--- src/AppBundle/Form/Type/CommentType.php
+++ src/AppBundle/Form/Type/CommentType.php
@@ -4,7 +4,7 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommentType extends AbstractType
 {
@@ -16,7 +16,7 @@ class CommentType extends AbstractType
       ));
   }
 
-  public function setDefaultsOptions(OptionsResolverInterface $resolver)
+  public function setDefaultsOptions(OptionsResolver $resolver)
   {
     $resolver->setDefaults(array(
       'data_class' => 'AppBundle\Entity\Comment'
--- src/AppBundle/Form/Type/NoteType.php
+++ src/AppBundle/Form/Type/NoteType.php
@@ -4,7 +4,7 @@ namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class NoteType extends AbstractType
 {
@@ -34,7 +34,7 @@ class NoteType extends AbstractType
       ));
   }
 
-  public function setDefaultsOptions(OptionsResolverInterface $resolver)
+  public function setDefaultsOptions(OptionsResolver $resolver)
   {
     $resolver->setDefaults(array(
       'data_class' => 'AppBundle\Entity\Notes'
