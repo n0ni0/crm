373dd93494e4384fae90d4e9ddd3711242b15156
Antonio Jiménez <ajimenez.bf@gmail.com>
2015-11-29 23:48:12 +0100

    Moved calendar-settings.js to web/js

---
+++ web/js/calendar-settings.js
@@ -0,0 +1,39 @@
+$(function () {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $('#calendar-holder').fullCalendar({
+        aspectRatio: 2,
+        header: {
+            left: 'prev, next',
+            center: 'title',
+            right: 'month, basicWeek, basicDay,'
+        },
+        lazyFetching: true,
+        timeFormat: {
+            // for agendaWeek and agendaDay
+            agenda: 'h:mmt',    // 5:00 - 6:30
+
+            // for all other views
+            '': 'h:mmt'         // 7p
+        },
+        //Set to True to show Calendar Week in seperate Row/Field of Calendar View
+        weekNumbers: false,
+        //Set to your Language to change the text, the first day of week etc acording to yout settings.
+        lang: 'es',
+        eventSources: [
+            {
+                url: Routing.generate('fullcalendar_loader'),
+                type: 'POST',
+                // A way to add custom filters to your event listeners
+                data: {
+                },
+                error: function() {
+                   //alert('There was an error while fetching Google Calendar!');
+                }
+            }
+        ]
+    });
+});
