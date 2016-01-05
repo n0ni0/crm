6b9ea0628efcb6f61d8affb5451fd32c28b9d25b
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-01-05 18:27:07 +0100

    User can view and edit plannings on tasks
    Fixs ORM relations on Entities
    Some visual changes

--- src/AppBundle/Controller/CalendarController.php
+++ src/AppBundle/Controller/CalendarController.php
@@ -1,101 +1,102 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller {
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\Type\PlanningType;
-use AppBundle\Security\CommentVoter;
-use AppBundle\Entity\Task;
-use AppBundle\Entity\Comment;
-use AppBundle\Entity\Calendar;
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+  use Symfony\Component\HttpFoundation\Request;
+  use AppBundle\Form\Type\PlanningType;
+  use AppBundle\Security\CommentVoter;
+  use AppBundle\Entity\Task;
+  use AppBundle\Entity\Comment;
+  use AppBundle\Entity\Calendar;
 
-class CalendarController extends Controller
-{
-  /**
-   * @route("/calendar", name="calendar")
-   *
-   */
-  public function showCalendarAction()
+  class CalendarController extends Controller
   {
-    return $this->render('calendar/calendar.html.twig');
-  }
-
-  /**
-   * @Route("/task/{task}/comment/{id}/planning/", name="planningComment")
-   * @ParamConverter("task", class="AppBundle:Task")
-   */
-  public function planninCommentAction(Request $request, Task $task, Comment $id)
-  {
-    $calendar = new Calendar();
-
-    $planning= $this->get('CalendarManager')->checkUserPlanning($id);
-
-    if($planning){
-      $this->addFlash(
-        'success',
-        'El comentario ya está planificado'
-      );
-      return $this->redirect($request->headers->get('referer'));
+    /**
+     * @route("/calendar", name="calendar")
+     *
+     */
+    public function showCalendarAction()
+    {
+      return $this->render('calendar/calendar.html.twig');
     }
 
-    $commentToPlanning = $this->get('CommentManager')->checkUserComment($id);
+    /**
+     * @Route("/task/{task}/comment/{id}/planning/", name="planningComment")
+     * @ParamConverter("task", class="AppBundle:Task")
+     */
+    public function planninCommentAction(Request $request, Task $task, Comment $id)
+    {
+      $calendar = new Calendar();
 
-    if(!$this->isGranted(CommentVoter::ATTRIBUTE_MODIFY, $commentToPlanning)){
-      throw $this->createAccessDeniedException('Comment not found or not allowed to edit');
+      $planning= $this->get('CalendarManager')->checkUserPlanning($id);
+
+      if($planning){
+        $this->addFlash(
+            'success',
+            'El comentario ya está planificado'
+        );
+        return $this->redirect($request->headers->get('referer'));
+      }
+
+      $commentToPlanning = $this->get('CommentManager')->checkUserComment($id);
+
+      if(!$this->isGranted(CommentVoter::ATTRIBUTE_MODIFY, $commentToPlanning)){
+        throw $this->createAccessDeniedException('Comment not found or not allowed to edit');
+      }
+
+      $form = $this->createForm(new PlanningType(), $calendar);
+      $form->handleRequest($request);
+
+      if($form->isValid()){
+        $data = $form->getData();
+        $data->setTitle($task->getTitle());
+        $data->setTask($task);
+        $data->setComment($id);
+        $this->get('CommentManager')->createComment($data);
+
+        $id = $task->getId();
+        return $this->redirectToRoute('task', array(
+            'id' => $task->getId()));
+      }
+
+      return $this->render('comments/planningComment.html.twig', array(
+          'form' => $form->createView(),
+      ));
     }
 
-    $form = $this->createForm(new PlanningType(), $calendar);
-    $form->handleRequest($request);
+    /**
+     * @Route("/task/{task}/comment/{id}/planning/edit/", name="editPlanningComment")
+     * @ParamConverter("task", class="AppBundle:Task")
+     * @ParamConverter("comment", class="AppBundle:Comment")
+     */
+    public function editPlanningCommentAction(Request $request, Task $task, Comment $id)
+    {
+      $planningToEdit = $this->get('CalendarManager')->checkUserPlanning($id);
 
-    if($form->isValid()){
-      $data = $form->getData();
-      $data->setTitle($task->getTitle());
-      $data->setTask($task);
-      $data->setComment($id);
-      $this->get('CommentManager')->createComment($data);
+      if(!$planningToEdit){
+        $this->addFlash(
+            'success',
+            'No se ha planificado ningún comentario'
+        );
+        return $this->redirect($request->headers->get('referer'));
+      }
 
-      $id = $task->getId();
-      return $this->redirectToRoute('task', array(
-        'id' => $task->getId()));
+      $form = $this->createForm(new PlanningType(), $planningToEdit);
+      $form->handleRequest($request);
+
+      if($form->isValid()){
+        $this->get('CalendarManager')->update();
+
+        return $this->redirectToRoute('task', array(
+            'id' => $task->getId()));
+      }
+
+      return $this->render('comments/planningComment.html.twig', array(
+          'form' => $form->createView(),
+      ));
     }
-
-    return $this->render('comments/planningComment.html.twig', array(
-      'form' => $form->createView(),
-    ));
-  }
-
-  /**
-   * @Route("/task/{task}/comment/{id}/planning/edit/", name="editPlanningComment")
-   * @ParamConverter("task", class="AppBundle:Task")
-   * @ParamConverter("comment", class="AppBundle:Comment")
-   */
-  public function editPlanningCommentAction(Request $request, Task $task, Comment $id)
-  {
-    $planningToEdit = $this->get('CalendarManager')->checkUserPlanning($id);
-
-    if(!$planningToEdit){
-      $this->addFlash(
-        'success',
-        'No se ha planificado ningún comentario'
-      );
-      return $this->redirect($request->headers->get('referer'));
-    }
-
-    $form = $this->createForm(new PlanningType(), $planningToEdit);
-    $form->handleRequest($request);
-
-    if($form->isValid()){
-      $this->get('CalendarManager')->update();
-
-      return $this->redirectToRoute('task', array(
-        'id' => $task->getId()));
-    }
-
-    return $this->render('comments/planningComment.html.twig', array(
-      'form' => $form->createView(),
-    ));
   }
 }
